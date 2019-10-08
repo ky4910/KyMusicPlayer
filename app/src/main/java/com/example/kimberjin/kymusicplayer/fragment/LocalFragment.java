3c12f5f53ded4bcbdc2479d8bad2f4f0f75c9f3c
ky4910 <kobe4910@qq.com>
2019-10-08 13:12:12 +0800

    complete list of local songs view

--- app/src/main/java/com/example/kimberjin/kymusicplayer/fragment/LocalFragment.java
+++ app/src/main/java/com/example/kimberjin/kymusicplayer/fragment/LocalFragment.java
@@ -8,6 +8,9 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,9 +20,11 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.kimberjin.kymusicplayer.R;
+import com.example.kimberjin.kymusicplayer.activity.MainActivity;
 import com.example.kimberjin.kymusicplayer.adapter.LocalMusicRvAdapter;
 import com.example.kimberjin.kymusicplayer.bean.LocalSong;
 import com.example.kimberjin.kymusicplayer.db.DbClient;
+import com.example.kimberjin.kymusicplayer.util.SpacesItemDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +39,7 @@ public class LocalFragment extends Fragment {
 
     private RecyclerView recyclerView;
     LocalMusicRvAdapter rvAdapter;
-    DbClient dbClient = new DbClient(getContext());
+    DbClient dbClient = new DbClient();
 
     private List<LocalSong> localSongsList = new ArrayList<>();
 
@@ -50,15 +55,27 @@ public class LocalFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_local_music, container, false);
-        localSongsList = dbClient.getLocalSongs();
+        localSongsList = dbClient.getLocalSongs(getContext());
         recyclerView = view.findViewById(R.id.rv_local_music);
+        Log.e(TAG, "List size is " + localSongsList.size());
         rvAdapter = new LocalMusicRvAdapter(getContext(), localSongsList);
+
+        //设置layoutManager,可以设置显示效果，是线性布局、grid布局，还是瀑布流布局
+        //参数是：上下文、列表方向（横向还是纵向）、是否倒叙
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),
+                LinearLayoutManager.VERTICAL, false));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        int spacingInPixels = 32;
+        recyclerView.addItemDecoration(new SpacesItemDecoration(spacingInPixels));
+
         recyclerView.setAdapter(rvAdapter);
         rvAdapter.setOnItemClickListener(new LocalMusicRvAdapter.OnItemClickListener() {
             @Override
             public void onItemClicked(View view, int position) {
                 Toast.makeText(getContext(), localSongsList.get(position).getTitle()
-                        + "selected!", Toast.LENGTH_SHORT).show();
+                        + " selected!", Toast.LENGTH_SHORT).show();
             }
         });
 

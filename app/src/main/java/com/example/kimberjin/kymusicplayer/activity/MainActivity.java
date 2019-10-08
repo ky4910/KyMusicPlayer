8fcfccdd66fff406e53a94b2cc010b5b9d298e73
ky4910 <kobe4910@qq.com>
2019-10-08 23:29:06 +0800

    add getAuthentication function

--- app/src/main/java/com/example/kimberjin/kymusicplayer/activity/MainActivity.java
+++ app/src/main/java/com/example/kimberjin/kymusicplayer/activity/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.kimberjin.kymusicplayer.activity;
 
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.MediaStore;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.kimberjin.kymusicplayer.R;
 import com.example.kimberjin.kymusicplayer.adapter.FragmentAdapter;
 import com.example.kimberjin.kymusicplayer.adapter.MyViewPagerAdapter;
+import com.example.kimberjin.kymusicplayer.bean.LocalSong;
 import com.example.kimberjin.kymusicplayer.fragment.HistoryFragment;
 import com.example.kimberjin.kymusicplayer.fragment.LocalFragment;
 import com.example.kimberjin.kymusicplayer.fragment.OnlineFragment;
@@ -24,12 +32,18 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private MyViewPagerAdapter pagerAdapter;
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE" };
+
     private String mTitles[] = {"本地音乐", "网络音乐", "播放历史"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getAuthentication();
         initView();
     }
 
@@ -67,5 +81,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void getAuthentication() {
+        try {
+            //检测是否有写的权限
+            int permission = ContextCompat.checkSelfPermission(MainActivity.this,
+                    "android.permission.READ_EXTERNAL_STORAGE");
+            // Manifest.permission.WRITE_EXTERNAL_STORAGE
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

ea722f72a5cb0a127ce93ba1b478ecee71473bd4
ky4910 <kobe4910@qq.com>
2019-10-08 18:33:14 +0800

    temp commit from company(2019.10.08)

---
+++ app/src/main/java/com/example/kimberjin/kymusicplayer/service/IPlayerService.java
@@ -0,0 +1,11 @@
+package com.example.kimberjin.kymusicplayer.service;
+
+/**
+ * Created by ky4910 on 2019/10/8
+ */
+public interface IPlayerService {
+    void onPlay();
+    void stop();
+    void onPlayNext();
+    void onPlayPrev();
+}

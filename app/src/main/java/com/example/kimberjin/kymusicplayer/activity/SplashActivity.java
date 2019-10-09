dfdb78ad405f0f3209720d5527ad28f4e95b718a
ky4910 <kobe4910@qq.com>
2019-10-09 13:44:06 +0800

    add booting page

---
+++ app/src/main/java/com/example/kimberjin/kymusicplayer/activity/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.kimberjin.kymusicplayer.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ky4910 on 2019/10/9
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*
+            runOnUiThread()这个方法的作用是将当前线程切换到主线程，
+            所以产生的作用和Handler传递消息的作用是相同的，但是
+            Handler要传递消息再接收消息，不如这个方法简便。
+         */
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
+

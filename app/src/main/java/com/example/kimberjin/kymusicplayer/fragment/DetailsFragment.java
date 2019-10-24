8ec7ebbc6a6d89669b0b9a3c895ee6e1311e0223
ky4910 <kobe4910@qq.com>
2019-10-24 22:37:12 +0800

    add onPlayNext and onPlayPrev

--- app/src/main/java/com/example/kimberjin/kymusicplayer/fragment/DetailsFragment.java
+++ app/src/main/java/com/example/kimberjin/kymusicplayer/fragment/DetailsFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
